b2b2c046338421e10642df3b4234c1615255ef1a
Ben Hall <ben@benhall.me.uk>
2016-06-18 16:31:20 +0100

    Updated views

--- src/TestModule.cs
+++ src/TestModule.cs
@@ -5,14 +5,14 @@ namespace Nancy.Demo.Hosting.Docker
         public TestModule()
         {
             Get["/"] = parameters => {
-                System.Console.WriteLine("Visit / on " + System.Environment.MachineName);
+                System.Console.WriteLine("Visit: / on " + System.Environment.MachineName);
                 return View["staticview", Request.Url];
             };
 
             Get["/machine"] = parameters =>
             {
-                System.Console.WriteLine("Visit /machine on " + System.Environment.MachineName);
-                return System.Environment.MachineName + "\r\n";
+                System.Console.WriteLine("Visit: /machine on " + System.Environment.MachineName);
+                return "Request processed by " + System.Environment.MachineName + "\r\n";
             };
         }
     }
